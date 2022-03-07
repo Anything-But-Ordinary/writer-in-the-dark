ea9df2a04c6f3bb3eaaaa926791ad7c3ad667b38
Ruby <LIU10032@163.com>
2022-03-07 12:06:39 +0800

    feat: user Resolver

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { UsersResolver } from './users.resolver';
+import { UsersSerivce } from './users.service';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [UsersSerivce, UsersResolver],
+})
 export class UsersModule {}
