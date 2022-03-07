2558942ede6474e54cc381a0c6424bda71f0f6b7
Ruby <LIU10032@163.com>
2022-03-07 14:13:27 +0800

    feat: check the user email exist or not

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateAccountInput } from './dtos/create-account.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -8,4 +9,23 @@ export class UsersSerivce {
   constructor(
     @InjectRepository(User) private readonly users: Repository<User>,
   ) {}
+
+  async createAccount({
+    email,
+    password,
+    who,
+  }: CreateAccountInput): Promise<string | undefined> {
+    try {
+      const exists = await this.users.findOne({ email });
+      if (exists) {
+        // make error
+        return 'There is a user with that email already';
+      }
+      await this.users.save(this.users.create({ email, password, who }));
+    } catch (e) {
+      return "Couldn't create account";
+    }
+    // hash the password
+    // ok
+  }
 }
