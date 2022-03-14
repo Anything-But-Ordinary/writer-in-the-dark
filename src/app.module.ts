23441035d5827971866420bc8530d5442e437a90
Ruby <LIU10032@163.com>
2022-03-14 09:04:10 +0800

    feat: clear some setting and ready to start again👋

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,67 +1,23 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import {
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-  RequestMethod,
-} from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import * as Joi from 'joi';
-import { JwtMiddleware } from './jwt/jwt.middleware';
-import { JwtModule } from './jwt/jwt.module';
-import { User } from './users/entities/user.entity';
-import { Verification } from './users/entities/verification.entities';
-import { FooResolver } from './users/foo.resolver';
-import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
-      ignoreEnvFile: process.env.NODE_ENV === 'prod',
-      validationSchema: Joi.object({
-        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
-        DB_HOST: Joi.string().required(),
-        DB_PORT: Joi.string().required(),
-        DB_USERNAME: Joi.string().required(),
-        DB_PWD: Joi.string().required(),
-        DB_NAME: Joi.string().required(),
-        PRIVATE_KEY: Joi.string().required(),
-      }),
-    }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PWD,
-      database: process.env.DB_NAME,
-      logging: true,
-      synchronize: true,
-      entities: [User, Verification],
     }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      // autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       autoSchemaFile: true,
-      context: ({ req }) => ({ user: req['user'] }),
     }),
-    JwtModule.forRoot({
-      privateKey: process.env.PRIVATE_KEY,
-    }),
-    UsersModule,
   ],
   controllers: [],
-  providers: [FooResolver],
+  providers: [],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(JwtMiddleware).forRoutes({
-      path: '/graphql',
-      method: RequestMethod.POST,
-    });
+    throw new Error('Method not implemented.');
   }
 }
